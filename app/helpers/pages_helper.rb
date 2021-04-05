51d14a14065404cc94a1fc2aa5a13d494929ef9e
vidhishweta01 <vidhishweta01@gmail.com>
2021-04-05 13:24:34 +0530

    removed the commented code and moved logic into post_helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,23 @@
 module PagesHelper
+  def sign_out
+    if user_signed_in?
+      link_to 'Sign out', destroy_user_session_path, method: :delete
+    else
+      link_to 'Sign Up', new_user_registration_path
+    end
+  end
+
+  def edit_profile
+    if user_signed_in?
+      link_to 'Edit Profile', edit_user_registration_path
+    else
+      link_to 'Sign in', new_user_session_path
+    end
+  end
+
+  def new_post
+    if user_signed_in?
+      link_to 'New Post', new_post_path
+    end
+  end
 end
