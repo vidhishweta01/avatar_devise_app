03e93207a8aafc0f53172c9a37e15df70eabca2c
vidhishweta01 <vidhishweta01@gmail.com>
2021-04-05 10:31:50 +0530

    added logic to post_helper.rb

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,19 @@
 module PostsHelper
+  def authentic_user1(post)
+    if signed_in? && post.user_id == current_user.id
+      link_to 'Edit', edit_post_path(post)
+    end
+  end
+
+  def authentic_user2(post)
+    if signed_in? && post.user_id == current_user.id
+      link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }
+    end
+  end
+
+  def members_only(post)
+    post.author if signed_in?
+
+  end
+
 end
