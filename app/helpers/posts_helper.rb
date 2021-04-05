3401fca1a014085eb9c9591de67371a0c4a1af44
vidhishweta01 <vidhishweta01@gmail.com>
2021-04-05 10:47:02 +0530

    fixed linters

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,19 +1,17 @@
 module PostsHelper
   def authentic_user1(post)
-    if signed_in? && post.user_id == current_user.id
+    if signed_in? && post.user_id == current_user.id # rubocop:disable Style/IfUnlessModifier, Style/GuardClause
       link_to 'Edit', edit_post_path(post)
     end
   end
 
   def authentic_user2(post)
-    if signed_in? && post.user_id == current_user.id
+    if signed_in? && post.user_id == current_user.id # rubocop:disable Style/IfUnlessModifier, Style/GuardClause
       link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }
     end
   end
 
   def members_only(post)
     post.author if signed_in?
-
   end
-
 end
